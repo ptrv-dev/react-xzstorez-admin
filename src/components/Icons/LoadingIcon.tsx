2bf13b088f50608af94ed7dcbd1abd65be6c3c26
ptrv-dev <petrov99119@gmail.com>
2023-03-07 21:38:02 +0200

    Icons component added

---
+++ src/components/Icons/LoadingIcon.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const LoadingIcon: React.FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 100 100"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="10"
+        r="35"
+        strokeDasharray="164.93361431346415 56.97787143782138"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        ></animateTransform>
+      </circle>
+    </svg>
+  );
+};
+
+export default LoadingIcon;
