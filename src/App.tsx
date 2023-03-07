6e4a4f5abcd9252e04ade973910ac340e2c0227d
ptrv-dev <petrov99119@gmail.com>
2023-03-07 23:23:56 +0200

    React-Router-Dom fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import appAxios from './axios';
 import LoadingPage from './pages/LoadingPage';
 import LoginPage from './pages/LoginPage';
@@ -8,16 +8,8 @@ import ProductsPage from './pages/ProductsPage';
 import { login, logout } from './store/slices/auth.slice';
 import { useAppDispatch, useAppSelector } from './store/store';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <ProductsPage />,
-  },
-  {
-    path: '*',
-    element: <h1>Page not found! Error 404</h1>,
-  },
-]);
+import style from './scss/App.module.scss';
+import ASide from './components/ASide';
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -44,7 +36,16 @@ const App: React.FC = () => {
 
   if (!isAuth) return <LoginPage />;
 
-  return <RouterProvider router={router} />;
+  return (
+    <main className={style.main}>
+      <ASide />
+      <div className={style.body}>
+        <Routes>
+          <Route index element={<ProductsPage />} />
+        </Routes>
+      </div>
+    </main>
+  );
 };
 
 export default App;
