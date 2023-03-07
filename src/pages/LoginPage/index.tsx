9ec93804b63f34e473940132a23169615640b61f
ptrv-dev <petrov99119@gmail.com>
2023-03-07 22:13:50 +0200

    Login Page - Functionality

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { isAxiosError } from 'axios';
+
+import appAxios from '../../axios';
 
 import UserIcon from '../../components/Icons/UserIcon';
 import LockIcon from '../../components/Icons/LockIcon';
 import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { useAppDispatch } from '../../store/store';
+import { login } from '../../store/slices/auth.slice';
 
 import style from './LoginPage.module.scss';
-import Button from '../../components/Button';
 
 interface LoginFormFields {
   username: string;
@@ -14,14 +20,33 @@ interface LoginFormFields {
 }
 
 const LoginPage: React.FC = () => {
+  const dispatch = useAppDispatch();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginFormFields>();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [message, setMessage] = React.useState<string>('');
 
-  const onSubmit: SubmitHandler<LoginFormFields> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<LoginFormFields> = async (data) => {
+    setLoading(true);
+    try {
+      await appAxios.post('/auth/login', data);
+      setMessage('');
+      dispatch(login());
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const statusCode = error.response?.status || 500;
+        console.error(error);
+
+        if (statusCode === 401) return setMessage('Incorrect credentials!');
+
+        alert('Something going wrong...');
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -54,7 +79,10 @@ const LoginPage: React.FC = () => {
           }}
           error={errors.password?.message || Boolean(errors.password)}
         />
-        <Button className={style.formButton}>Login</Button>
+        {message && <p className={style.formWarning}>{message}</p>}
+        <Button className={style.formButton} loading={loading}>
+          Login
+        </Button>
       </form>
     </div>
   );
