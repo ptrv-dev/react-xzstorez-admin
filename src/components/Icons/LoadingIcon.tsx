a1e74f6e8e203e65b7c82494a720c8dbb7c90be9
ptrv-dev <petrov99119@gmail.com>
2023-03-07 22:27:29 +0200

    Icons components add width and height props

--- src/components/Icons/LoadingIcon.tsx
+++ src/components/Icons/LoadingIcon.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const LoadingIcon: React.FC = () => {
+interface IconProps {
+  width?: number;
+  height?: number;
+}
+
+const LoadingIcon: React.FC<IconProps> = ({ width = 24, height = 24 }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24px"
-      height="24px"
+      width={width}
+      height={height}
       viewBox="0 0 100 100"
     >
       <circle
--- src/components/Icons/LockIcon.tsx
+++ src/components/Icons/LockIcon.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const LockIcon = () => {
+interface IconProps {
+  width?: number;
+  height?: number;
+}
+
+const LockIcon:React.FC<IconProps> = ({width=24,height=24}) => {
   return (
     <svg
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
