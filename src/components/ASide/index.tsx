bfb4f7976550ecd0e6180e12321cf4c13b5e099d
ptrv-dev <petrov99119@gmail.com>
2023-03-07 23:39:11 +0200

    ASide component

---
+++ src/components/ASide/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import FolderIcon from '../Icons/FolderIcon';
+import GridIcon from '../Icons/GridIcon';
+import SettingsIcon from '../Icons/SettingsIcon';
+import ShoppingBagIcon from '../Icons/ShoppingBagIcon';
+import BriefCaseIcon from '../Icons/BriefCaseIcon';
+
+import style from './ASide.module.scss';
+
+interface NavigationItemProps {
+  icon: React.ReactElement;
+  title: string;
+  href: string;
+}
+
+const NavigationItem: React.FC<NavigationItemProps> = ({
+  icon,
+  title,
+  href,
+}) => {
+  return (
+    <NavLink to={href} className={style.navigationItem}>
+      <>
+        {icon}
+        {title}
+      </>
+    </NavLink>
+  );
+};
+
+const navigation = [
+  {
+    icon: <FolderIcon />,
+    title: 'Products',
+    href: '/',
+  },
+  {
+    icon: <GridIcon />,
+    title: 'Categories',
+    href: '/categories',
+  },
+  {
+    icon: <BriefCaseIcon />,
+    title: 'Brands',
+    href: '/brands',
+  },
+  {
+    icon: <ShoppingBagIcon />,
+    title: 'Orders',
+    href: '/orders',
+  },
+  {
+    icon: <SettingsIcon />,
+    title: 'Settings',
+    href: '/settings',
+  },
+];
+
+const ASide: React.FC = () => {
+  return (
+    <div className={style.aside}>
+      <h4 className={style.asideTitle}>Admin Panel</h4>
+      <nav className={style.asideNav}>
+        <ul className={style.asideList}>
+          {navigation.map((item, idx) => (
+            <li className={style.asideItem} key={idx}>
+              <NavigationItem
+                icon={item.icon}
+                title={item.title}
+                href={item.href}
+              />
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default ASide;
